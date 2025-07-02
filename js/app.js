55fd25a72a9ce5bb4872022067ce60eafbc4ebd5
timurantonyan <timurantonyan96@gmail.com>
2025-07-03 01:08:10 +0300

    create error

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.getElementById('burger-toggle').addEventListener('change', function() {
+    const navMenu = document.querySelector('.nav__list');
+    if (this.checked) {
+        navMenu.classList.add('nav__list--open');
+    } else {
+        navMenu.classList.remove('nav__list--open');
+    }
+});
