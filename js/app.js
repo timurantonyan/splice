85d0f7dd7f06b94acdbdc686a427d58f3b3e76ae
timurantonyan <timurantonyan96@gmail.com>
2025-07-09 00:41:51 +0300

    create scroll

--- js/app.js
+++ js/app.js
@@ -8,4 +8,13 @@ document.getElementById('burger-toggle').addEventListener('change', function() {
     }
 });
 
+const header = document.querySelector('.header__navigation');
+const stickyOffset = header.offsetTop;
 
+window.addEventListener('scroll', () => {
+    if (window.scrollY > stickyOffset) {
+        header.classList.add('.sticky');
+    } else {
+        header.classList.remove('.sticky');
+    }
+});
