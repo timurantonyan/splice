3913e9931b35ce961d7ae730b590e8c1ed68e8fb
timurantonyan <timurantonyan96@gmail.com>
2025-07-09 18:17:59 +0300

    create slider

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+import Swiper from 'swiper/bundle';
 document.getElementById('burger-toggle').addEventListener('change', function() {
     const navMenu = document.querySelector('.nav__list');
     if (this.checked) {
@@ -16,4 +16,29 @@ window.addEventListener('scroll', function() {
         header.classList.remove('sticky');
     }
     console.log('Scroll position:', window.scrollY, 'px')
+});
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
 });
